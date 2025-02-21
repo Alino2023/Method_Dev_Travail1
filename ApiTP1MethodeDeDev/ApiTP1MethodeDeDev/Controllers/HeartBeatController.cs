093eeb1f2211bac442d0f4bdbc747dcde5aa0564
FiliwaK <157498510+FiliwaK@users.noreply.github.com>
2025-02-21 14:30:06 -0500

    squellette Api ok

---
+++ ApiTP1MethodeDeDev/ApiTP1MethodeDeDev/Controllers/HeartBeatController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiTP1MethodeDeDev.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeartBeatController : ControllerBase
+    {
+        // GET: api/<HeartBeatController>
+        [HttpGet]
+        public IActionResult GetApiHeartbeat()
+        {
+            return Ok();
+        }
+
+        
+    }
+}
