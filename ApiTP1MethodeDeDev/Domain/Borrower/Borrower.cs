02644d16b6550b8692eaec8f6bb05aa4d400a11d
FiliwaK <157498510+FiliwaK@users.noreply.github.com>
2025-03-12 16:19:32 -0400

    class borrower and appDbContext ok

---
+++ ApiTP1MethodeDeDev/Domain/Borrower/Borrower.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Borrower
+{
+    public class Borrower
+    {
+        [Required]
+        [MinLength(9)]
+        [MaxLength(11)]
+        [Description("User's Social insurance Number")]
+        //[Example("123-456-789")]
+        private string Sin { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        [Description("User's FirstName")]
+        private string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        [Description("User's LastName")]
+        private string LastName { get; set; }
+
+        [Required]
+        [MaxLength(11)]
+        [Description("User's Phone")]
+        private string Phone { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        [Description("User's Email")]
+        private string Email { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        [Description("User's Address")]
+        private string Address { get; set; }
+
+        public Borrower()
+        {
+        }
+
+    }
+}
---
+++ ApiTP1MethodeDeDev/Infrastructure/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Borrower;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Borrower> Borrowers { get; set; } = null!;
+
+
+    }
+}
