6b77a86dbae37e46ea62f8168db023dda36f370b
FiliwaK <157498510+FiliwaK@users.noreply.github.com>
2025-03-12 16:59:50 -0400

    Update Borrower.cs

--- ApiTP1MethodeDeDev/Domain/Borrower/Borrower.cs
+++ ApiTP1MethodeDeDev/Domain/Borrower/Borrower.cs
@@ -15,32 +15,32 @@ namespace Domain.Borrower
         [MaxLength(11)]
         [Description("User's Social insurance Number")]
         //[Example("123-456-789")]
-        private string Sin { get; set; }
+        public string Sin { get; set; }
 
         [Required]
         [MaxLength(255)]
         [Description("User's FirstName")]
-        private string FirstName { get; set; }
+        public string FirstName { get; set; }
 
         [Required]
         [MaxLength(255)]
         [Description("User's LastName")]
-        private string LastName { get; set; }
+        public string LastName { get; set; }
 
         [Required]
         [MaxLength(11)]
         [Description("User's Phone")]
-        private string Phone { get; set; }
+        public string Phone { get; set; }
 
         [Required]
         [MaxLength(255)]
         [Description("User's Email")]
-        private string Email { get; set; }
+        public string Email { get; set; }
 
         [Required]
         [MaxLength(255)]
         [Description("User's Address")]
-        private string Address { get; set; }
+        public string Address { get; set; }
 
         public Borrower()
         {
