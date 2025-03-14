59c86daacc79c44c1d995646e1d72bf612ef3ddc
FiliwaK <157498510+FiliwaK@users.noreply.github.com>
2025-03-14 15:28:00 -0400

    ajout de task pour async

--- ApiTP1MethodeDeDev/Infrastructure/InMemoryBorrowerRepository.cs
+++ ApiTP1MethodeDeDev/Infrastructure/InMemoryBorrowerRepository.cs
@@ -4,29 +4,66 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Domain.Borrower;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure
 {
     public class InMemoryBorrowerRepository : IBorrowerRepository
     {
-        public int Add(Borrower borrower)
+        private readonly AppDbContext _appDbContext;
+
+        public InMemoryBorrowerRepository(AppDbContext appDbContext)
         {
-            throw new NotImplementedException();
+            _appDbContext = appDbContext;
         }
 
-        public List<Borrower> GetAll()
-        {
-            throw new NotImplementedException();
-        }
+        //public int Add(Borrower borrower)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        public Borrower GetBySin(string sin)
-        {
-            throw new NotImplementedException();
-        }
+        //public List<Borrower> GetAll()
+        //{
+        //    return _appDbContext.Borrowers.Select(b => new Borrower(b.Sin, b.FirstName, b.LastName, b.Phone, b.Email, b.Address)).ToList();
+
+        //}
+
+        //public Borrower GetBySin(string sin)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
         public void Update(Borrower borrower)
         {
             throw new NotImplementedException();
         }
+
+        Task<int> IBorrowerRepository.Add(Borrower borrower)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<List<Borrower>> IBorrowerRepository.GetAll()
+        {
+            return _appDbContext.Borrowers.Select(b => BorrowerToEntity(b))
+                .ToListAsync();
+            throw new NotImplementedException();
+        }
+
+        Task<Borrower> IBorrowerRepository.GetBySin(string sin)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static BorrowerEntity BorrowerToEntity(Borrower borrower) =>
+           new BorrowerEntity
+           {
+               Sin = borrower.Sin,
+               FirstName = borrower.FirstName,
+               LastName = borrower.LastName,
+               Phone = borrower.Phone,
+               Email = borrower.Email,
+               Address = borrower.Address
+           };
     }
 }
