4488921630abd02082be5f9b3a55121b15787e9b
FiliwaK <157498510+FiliwaK@users.noreply.github.com>
2025-03-14 15:28:44 -0400

    Create BorrowerService.cs

---
+++ ApiTP1MethodeDeDev/Domain/Borrower/BorrowerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Borrower
+{
+    public class BorrowerService : IBorrowerService
+    {
+
+        Task<IList<Borrower>> IBorrowerService.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
