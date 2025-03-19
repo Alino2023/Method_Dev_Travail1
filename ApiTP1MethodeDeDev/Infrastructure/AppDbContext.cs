5cf1fafef8e65699d3f76af51985593ed3078298
FiliwaK <157498510+FiliwaK@users.noreply.github.com>
2025-03-19 16:45:06 -0400

    Update AppDbContext.cs

--- ApiTP1MethodeDeDev/Infrastructure/AppDbContext.cs
+++ ApiTP1MethodeDeDev/Infrastructure/AppDbContext.cs
@@ -14,6 +14,18 @@ namespace Infrastructure
         {
         }
         public DbSet<BorrowerEntity> Borrowers { get; set; } = null!;
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BorrowerEntity>().HasKey(b => b.Sin);
+
+            modelBuilder.Entity<BorrowerEntity>(b => b.HasData(
+                new BorrowerEntity { Sin = "157489632", FirstName = "Zakaria", LastName = "Morjani", Phone = "4182571159", Email = "zakaria@gmail.com", Address = "le lac fortain" }
+            ));
+
+            base.OnModelCreating(modelBuilder);
+        }
 
 
     }
