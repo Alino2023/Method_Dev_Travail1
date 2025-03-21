83dd259fd2a19a7ab92be402cf0d8f9087701830
DOGBE Etchri Semane <124226926+Alino2023@users.noreply.github.com>
2025-03-21 16:10:10 -0400

    Class Laon created
    Client's loan from our the bank institute

---
+++ ApiTP1MethodeDeDev/Domain/Loan/Loan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Loan
+{
+    public class Loan
+    {
+        public int IdLoan { get; set; }
+        public decimal Amount { get; set; }
+        public decimal InterestRate { get; set; }
+        public int DurationInMonths { get; set; }
+        public StatusLoan Status { get; set; } = StatusLoan.InProgress;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        // public Borrower TheBorrower { get; set; }
+
+        public enum StatusLoan
+        {
+            Pending,    // En attente
+            Approved,   // Approuvé
+            Rejected,   // Rejeté
+                        // Disbursed,  // Décaissé
+            Closed,     // Fermé/Remboursé
+            Overdue,     // En retard
+            InProgress,   // En cours
+            Paid        // Payé
+
+        }
+        public decimal RemainingAmount { get; set; }
+    }
+}
