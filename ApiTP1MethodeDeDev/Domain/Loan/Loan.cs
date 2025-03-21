b62516139a67a6717d5427ed81bb5291a46ab620
DOGBE Etchri Semane <124226926+Alino2023@users.noreply.github.com>
2025-03-21 19:05:07 -0400

    class Loan structure modied

--- ApiTP1MethodeDeDev/Domain/Loan/Loan.cs
+++ ApiTP1MethodeDeDev/Domain/Loan/Loan.cs
@@ -12,24 +12,12 @@ namespace Domain.Loan
         public decimal Amount { get; set; }
         public decimal InterestRate { get; set; }
         public int DurationInMonths { get; set; }
-        public StatusLoan Status { get; set; } = StatusLoan.InProgress;
+        public StatusLoan Status { get; set; } = StatusLoan.Pending;
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-
-        // public Borrower TheBorrower { get; set; }
-
-        public enum StatusLoan
-        {
-            Pending,    // En attente
-            Approved,   // Approuvé
-            Rejected,   // Rejeté
-                        // Disbursed,  // Décaissé
-            Closed,     // Fermé/Remboursé
-            Overdue,     // En retard
-            InProgress,   // En cours
-            Paid        // Payé
-
-        }
         public decimal RemainingAmount { get; set; }
+
+        //public Borrower TheBorrower { get; set; }
+
     }
 }
