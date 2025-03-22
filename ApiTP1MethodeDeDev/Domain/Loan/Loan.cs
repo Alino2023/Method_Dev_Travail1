9a2184d5da768792126f4dcc11b6b81133d18d90
DOGBE Etchri Semane <124226926+Alino2023@users.noreply.github.com>
2025-03-22 11:27:11 -0400

    Update Loan.cs

--- ApiTP1MethodeDeDev/Domain/Loan/Loan.cs
+++ ApiTP1MethodeDeDev/Domain/Loan/Loan.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,16 +12,62 @@ namespace Domain.Loan
 {
     public class Loan
     {
+        [Key]
+        [Required]
+        [Description("It is an unique integer number of a loan ")]
         public int IdLoan { get; set; }
+
+        [Required]
+        [Description("The whole amount of the loan")]
         public decimal Amount { get; set; }
+
+        [Required]
+        [Description("The rate of interests")]
         public decimal InterestRate { get; set; }
+
+        [Required]
+        [Description("Specify the duration of the paiements in months")]
         public int DurationInMonths { get; set; }
+
+        [NotMapped]
+        [Description("The state of the loan. By default it is Pending")]
         public StatusLoan Status { get; set; } = StatusLoan.Pending;
+
+        [Required]
+        [Description ("The date when paiements will start")]
         public DateTime StartDate { get; set; }
+
+        [Required]
+        [Description("The end date of paiements")]
         public DateTime EndDate { get; set; }
+
+        [Required]
+        [Description("The exact and precise amount that the borrower undertakes to repay to the bank as part of his loan")]
         public decimal RemainingAmount { get; set; }
+
+        [Required]
         public string BorrowerSin {  get; set; }
+
+        [Required]
         public BorrowerService TheBorrower { get; set; }
 
+        public List<decimal> Loans { get; set; } = new List<decimal>();
+        public Loan(int idLoan, decimal amount, decimal interestRate, int durationInMonths, StatusLoan status, DateTime startDate, DateTime endDate, decimal remainingAmount, string borrowerSin, BorrowerService theBorrower)
+        {
+            IdLoan = idLoan;
+            Amount = amount;
+            InterestRate = interestRate;
+            DurationInMonths = durationInMonths;
+            Status = status;
+            StartDate = startDate;
+            EndDate = endDate;
+            RemainingAmount = remainingAmount;
+            BorrowerSin = borrowerSin;
+            TheBorrower = theBorrower;
+        }
+
+        public Loan()
+        {
+        }
     }
 }
