c5c1e5f3df6e64b6b15e2b7adb70068e4138de87
DOGBE Etchri Semane <124226926+Alino2023@users.noreply.github.com>
2025-03-22 06:25:09 -0400

    Reapply "Merge pull request #3 from Alino2023/2232163_Semane"
    This reverts commit 5d92b8d56428f7716ea51d3b73f4270964b55189.

---
+++ ApiTP1MethodeDeDev/ApiTP1MethodeDeDev/Controllers/LoanController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTP1MethodeDeDev.Controllers
+{
+    [ApiController]
+    [Route("api/loans")]
+    public class LoanController : ControllerBase
+
+    { private readonly LoanRepository _loanRepository;
+
+        public LoanController(LoanRepository loanRepository)
+        {
+            _loanRepository = loanRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateLoan([FromBody] Loan loan)
+        {
+            if (loan == null)
+                return BadRequest("Les données du prêt sont invalides.");
+
+            await _loanRepository.AddLoanAsync(loan);
+            return Ok(loan);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetLoans()
+        {
+            var loans = await _loanRepository.GetLoansAsync();
+            return Ok(loans);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetLoanById(int id)
+        {
+            var loan = await _loanRepository.GetLoanByIdAsync(id);
+            if (loan == null) return NotFound();
+            return Ok(loan);
+        }
+
+    }
+}
---
+++ ApiTP1MethodeDeDev/Domain/Loan/LoanService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Loan
+{
+    public class LoanService : ILoanService
+    {
+        private readonly ILoanRepository _loanRepository;
+
+        public LoanService(ILoanRepository loanRepository)
+        {
+            _loanRepository = loanRepository;
+        }
+       
+        public string Create(Loan loan)
+        {
+            throw new NotImplementedException();
+        }
+        public IList<Loan> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Loan GetById(int idLoan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Loan loan)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
