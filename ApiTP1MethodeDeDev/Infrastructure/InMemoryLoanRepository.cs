12090690a90fc4bf8c1bef2ba254dc5b7c6f5b4a
DOGBE Etchri Semane <124226926+Alino2023@users.noreply.github.com>
2025-03-22 07:53:50 -0400

    The class InMemoryLoanRepository is created without methods implementation

---
+++ ApiTP1MethodeDeDev/Infrastructure/InMemoryLoanRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Loan;
+
+namespace Infrastructure
+{
+    public class InMemoryLoanRepository : ILoanRepository
+    {
+        private readonly AppDbContext _context;
+
+        public InMemoryLoanRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public object AddLoanAsync(Loan loan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Create(Loan loan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Loan> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Loan GetByIdLoan(int idLoan)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Loan loan)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
