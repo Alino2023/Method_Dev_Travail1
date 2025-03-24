f4d2d25bf7a53e2182de33d19fe40e6b71ca6aab
DOGBE Etchri Semane <124226926+Alino2023@users.noreply.github.com>
2025-03-23 22:47:45 -0400

    Update LoanEntity.cs

--- ApiTP1MethodeDeDev/Infrastructure/LoanEntity.cs
+++ ApiTP1MethodeDeDev/Infrastructure/LoanEntity.cs
@@ -1,13 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain.Borrower;
+using Domain.Loan;
 
 namespace Infrastructure
 {
     public class LoanEntity
     {
-        public object Status { get; internal set; }
+        [Key]
+        [Required]
+        [Description("It is an unique integer number of a loan ")]
+        public int IdLoan { get; set; }
+
+        [Required]
+        [Description("The whole amount of the loan")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [Description("The rate of interests")]
+        public decimal InterestRate { get; set; }
+
+        [Required]
+        [Description("Specify the duration of the paiements in months")]
+        public int DurationInMonths { get; set; }
+
+        [NotMapped]
+        [Description("The state of the loan. By default it is Pending")]
+        public StatusLoan Status { get; set; } = StatusLoan.Pending;
+
+        [Required]
+        [Description("The date when paiements will start")]
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        [Required]
+        [Description("The end date of paiements")]
+        public DateTime EndDate { get; set; } 
+
+        [Required]
+        [Description("The exact and precise amount that the borrower undertakes to repay to the bank as part of his loan")]
+        public decimal RemainingAmount { get; set; }
+
+        [Required]
+        public string BorrowerSin { get; set; }
+
+        [Required]
+        public BorrowerService TheBorrower { get; set; }
+      
     }
 }
