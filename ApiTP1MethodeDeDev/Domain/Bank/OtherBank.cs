6a5cde98ba79b354d771817d2fc61855e1f7565e
Zakaria-morjani <157501293+Zakaria-morjani@users.noreply.github.com>
2025-03-26 16:02:05 -0400

    Adding Other banks loans

---
+++ ApiTP1MethodeDeDev/Domain/Bank/OtherBank.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Bank
+{
+    public class OtherBank
+    {
+
+        [Required]
+        [Description("Loan mensuality")]
+        decimal Mensuality { get; set; }
+
+        [Required]
+        [Description("Loan Remaining Balance")]
+        decimal RemainingBalance {  get; set; }
+        [Required]
+        [Description("Reason for the loan ")]
+        string Reason { get; set; }
+    }
+}
