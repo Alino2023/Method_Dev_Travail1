8a2baba0ab076a9d5dc39a271b58cd575dc68c3f
Zakaria-morjani <157501293+Zakaria-morjani@users.noreply.github.com>
2025-03-26 16:02:45 -0400

    update the job class

--- ApiTP1MethodeDeDev/Domain/Job/Job.cs
+++ ApiTP1MethodeDeDev/Domain/Job/Job.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Emploi
 {
-    internal class Job
+    public class Job
     {
+        public int JobId {  get; set; } 
+        public string InstitutionName {  get; set; }
+        DateTime StartingDate { get; set; }
+        DateTime EndingDate { get; set; }
+        Decimal MentualSalary {  get; set; }
+
+        
     }
 }
