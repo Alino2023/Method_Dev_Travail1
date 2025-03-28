d4dfc4e08c4f1663a4464d6605991bc65cc40042
DOGBE Etchri Semane <124226926+Alino2023@users.noreply.github.com>
2025-03-28 14:40:25 -0400

    Loan Dtos created
    LoanRequest
    LoanRepsponse

--- ApiTP1MethodeDeDev/ApiTP1MethodeDeDev/Controllers/LoanController.cs
+++ ApiTP1MethodeDeDev/ApiTP1MethodeDeDev/Controllers/LoanController.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Loans;
+﻿using ApiTP1MethodeDeDev.Dtos;
+using Domain.Loans;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiTP1MethodeDeDev.Controllers
@@ -7,14 +8,26 @@ namespace ApiTP1MethodeDeDev.Controllers
     [Route("api/loans")]
     public class LoanController : ControllerBase
 
-    { private readonly ILoanService _loanService;
+    { 
+        private readonly ILoanService _loanService;
 
         public LoanController(ILoanService loanService)
         {
             _loanService = loanService;
         }
 
-        
+        public IEnumerable<LoanReponse> Get()
+        {
+            return _borrowerService.GetAll().Select(b =>
+                new BorrowerReponse
+                {
+                    FirstName = b.FirstName,
+                    LastName = b.LastName,
+                    Phone = b.Phone,
+                    Email = b.Email,
+                    Address = b.Address
+                });
+        }
 
     }
 }
---
+++ ApiTP1MethodeDeDev/ApiTP1MethodeDeDev/Dtos/Loan/LoanRequest.cs
@@ -0,0 +1,51 @@
+﻿using Domain.Borrowers;
+using Domain.Loans;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace ApiTP1MethodeDeDev.Dtos.Loan
+{
+    public class LoanRequest
+    {
+        [Key]
+        [Required]
+        [MinLength(1)]
+        [Description("It is an unique integer number of a loan ")]
+        public int IdLoan { get; set; }
+
+        [Required]
+        [Description("The whole amount of the loan")]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [Description("The rate of interests")]
+        public decimal InterestRate { get; set; }
+
+        [Required]
+        [Description("Specify the duration of the paiements in months")]
+        public int DurationInMonths { get; set; }
+
+        [Required]
+        [Description("The state of the loan. By default it is Pending")]
+        public StatusLoan Status { get; set; } = StatusLoan.Pending;
+
+        [Required]
+        [Description("The date when paiements will start")]
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        [Required]
+        [Description("The end date of paiements")]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        [Description("The exact and precise amount that the borrower undertakes to repay to the bank as part of his loan")]
+        public decimal RemainingAmount { get; set; }
+
+        [Required]
+        public Borrower TheBorrower { get; set; }
+
+        public List<decimal> Loans { get; set; } = new List<decimal>();
+
+    }
+}
