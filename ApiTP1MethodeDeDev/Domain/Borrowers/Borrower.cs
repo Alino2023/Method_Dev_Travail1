82622f3ebf118b6d3bc79c684b702e5b468f7b24
Zakaria-morjani <157501293+Zakaria-morjani@users.noreply.github.com>
2025-03-28 15:31:39 -0400

    update of the function to ClassifyRisk

--- ApiTP1MethodeDeDev/Domain/Borrowers/Borrower.cs
+++ ApiTP1MethodeDeDev/Domain/Borrowers/Borrower.cs
@@ -65,7 +65,7 @@ namespace Domain.Borrowers
 
         [Required]
         [Description("List of monthly payments from other banks")]
-        public List<OtherBank> OtherBankLoans { get; set; } = new List<OtherBank>();
+        public List<OtherBankLoan> OtherBankLoans { get; set; } = new List<OtherBankLoan>();
         public List<Job> EmploymentHistory { get; set; } = new List<Job>();
         public List<Loan> Loans { get; set; } = new List<Loan>();
 
@@ -90,11 +90,35 @@ namespace Domain.Borrowers
             Job currentJob = EmploymentHistory.OrderByDescending(job => job.StartingDate).FirstOrDefault();
             bool currentJobIsLessThan12Months = ((currentJob != null) && ((DateTime.Now - currentJob.StartingDate).TotalDays < 365));
 
-            if (Had_Bankrupty_In_Last_Six_Years || Equifax_Result < 650 || DebtRatio > 0.40m)
+            return  IsHighRisk(jobsInLastTwoYears, currentJob, currentJobIsLessThan12Months) ? "High Risk" :
+                    IsMediumRisk(jobsInLastTwoYears, currentJob, currentJobIsLessThan12Months) ? "Medium Risk" :
+                    IsLowRisk(jobsInLastTwoYears, currentJob, currentJobIsLessThan12Months) ? "Low Risk" :
+                    "Error in the classification of risk";
+        }
+        private bool IsHighRisk(int jobsInLastTwoYears, Job currentJob, bool currentJobIsLessThan12Months)
+        {
+            if (Had_Bankrupty_In_Last_Six_Years || Equifax_Result < 650 || DebtRatio >= 0.4m)
             {
-                return "Risque élevé";
+                return true;
             }
-            return "";
+            return false;
+        }
+        private bool IsMediumRisk(int jobsInLastTwoYears, Job currentJob, bool currentJobIsLessThan12Months)
+        {
+            if (!Had_Bankrupty_In_Last_Six_Years && Equifax_Result >= 650 && Equifax_Result < 750 && DebtRatio >= 0.25m && DebtRatio < 0.4m && NumberOfLatePayements <= 1
+                && (jobsInLastTwoYears >= 3 || currentJobIsLessThan12Months))
+            {
+                return true ;
+            }
+            return false;
+        }
+        private bool IsLowRisk(int jobsInLastTwoYears, Job currentJob, bool currentJobIsLessThan12Months)
+        {
+            if (!Had_Bankrupty_In_Last_Six_Years && Equifax_Result > 750 && NumberOfLatePayements == 0 && DebtRatio < 0.25m && jobsInLastTwoYears <= 2 && !currentJobIsLessThan12Months)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
