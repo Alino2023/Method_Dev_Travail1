549e42bf927f9f594f464023eaf8259ddb01f760
FiliwaK <157498510+FiliwaK@users.noreply.github.com>
2025-04-02 16:17:50 -0400

    Create JobRequest.cs

---
+++ ApiTP1MethodeDeDev/ApiTP1MethodeDeDev/Dtos/JobRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiTP1MethodeDeDev.Dtos
+{
+    public class JobRequest
+    {
+        public string InstitutionName { get; set; }
+        public DateTime StartingDate { get; set; }
+        public DateTime EndingDate { get; set; }
+        public Decimal MentualSalary { get; set; }
+    }
+}
