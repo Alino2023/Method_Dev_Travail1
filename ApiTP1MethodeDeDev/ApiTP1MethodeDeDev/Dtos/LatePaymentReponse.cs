fc94ec98e37bbe1add259144c48fe6fac6dfe1bd
FiliwaK <157498510+FiliwaK@users.noreply.github.com>
2025-04-02 16:17:48 -0400

    Create LatePaymentReponse.cs

---
+++ ApiTP1MethodeDeDev/ApiTP1MethodeDeDev/Dtos/LatePaymentReponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiTP1MethodeDeDev.Dtos
+{
+    public class LatePaymentReponse
+    {
+        public int LatePaymentId { get; set; }
+
+        public DateTime LatePaymentDate { get; set; }
+    }
+}
