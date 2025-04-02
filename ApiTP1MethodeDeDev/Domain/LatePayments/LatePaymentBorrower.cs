a7da25e10b55cb5f0c5f4306e8d7e9bff652b637
FiliwaK <157498510+FiliwaK@users.noreply.github.com>
2025-04-02 16:17:08 -0400

    Update LatePaymentBorrower.cs

--- ApiTP1MethodeDeDev/Domain/LatePayments/LatePaymentBorrower.cs
+++ ApiTP1MethodeDeDev/Domain/LatePayments/LatePaymentBorrower.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Domain.LatePayment
 {
     public class LatePaymentBorrower
     {
+        [Key]
         public int LatePaymentId { get; set; }
 
         public DateTime LatePaymentDate {  get; set; }
