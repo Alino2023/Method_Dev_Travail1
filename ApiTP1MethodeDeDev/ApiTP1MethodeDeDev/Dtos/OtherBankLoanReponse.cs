441052b643451f7c8fc7bc329800c9b06d2895f4
FiliwaK <157498510+FiliwaK@users.noreply.github.com>
2025-04-02 16:17:45 -0400

    Create OtherBankLoanReponse.cs

---
+++ ApiTP1MethodeDeDev/ApiTP1MethodeDeDev/Dtos/OtherBankLoanReponse.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace ApiTP1MethodeDeDev.Dtos
+{
+    public class OtherBankLoanReponse
+    {
+
+        public int BankId { get; set; }
+
+        public string BankName { get; set; }
+
+        [Description("Loan mensuality")]
+        public decimal Mensuality { get; set; }
+
+        [Description("Loan Remaining Balance")]
+        public decimal RemainingBalance { get; set; }
+
+        [Description("Reason for the loan ")]
+        public string Reason { get; set; }
+    }
+}
