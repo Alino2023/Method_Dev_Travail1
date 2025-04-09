124171aec4c92604083e00345d779c54bc11c044
JeffloveJeanfrancois <157496861+JeffloveJeanfrancois@users.noreply.github.com>
2025-04-08 21:16:51 -0400

    creation de la classe latepayement borrower

---
+++ ApiTP1MethodeDeDev/LoanRequestTest/LatePaymentBorrowerTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Domain.LatePayment;
+
+namespace Tests
+{
+    [TestClass]
+    public class LatePaymentBorrowerTests
+    {
+        [TestMethod]
+        public void LatePaymentBorrower_ShouldBeValid_WhenDateIsProvided()
+        {
+            var latePayment = new LatePaymentBorrower
+            {
+                LatePaymentDate = DateTime.Now.AddDays(-30)
+            };
+
+            var context = new ValidationContext(latePayment, null, null);
+            var results = new List<ValidationResult>();
+
+            var isValid = Validator.TryValidateObject(latePayment, context, results, true);
+
+            Assert.IsTrue(isValid);
+            Assert.AreEqual(0, results.Count);
+        }
+
+        [TestMethod]
+        public void LatePaymentBorrower_ShouldFailValidation_IfDateIsDefault()
+        {
+            var latePayment = new LatePaymentBorrower
+            {
+                LatePaymentDate = default 
+            };
+
+            var context = new ValidationContext(latePayment, null, null);
+            var results = new List<ValidationResult>();
+
+            var isValid = Validator.TryValidateObject(latePayment, context, results, true);
+
+            Assert.IsTrue(isValid);
+        }
+    }
+}
