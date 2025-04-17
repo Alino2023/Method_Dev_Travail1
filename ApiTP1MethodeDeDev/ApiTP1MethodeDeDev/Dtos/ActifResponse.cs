954d2d05103634f8aba36736b4a8bdbab8d378be
Zakaria-morjani <157501293+Zakaria-morjani@users.noreply.github.com>
2025-04-17 16:25:33 -0400

    Create ActifResponse.cs

---
+++ ApiTP1MethodeDeDev/ApiTP1MethodeDeDev/Dtos/ActifResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiTP1MethodeDeDev.Dtos
+{
+    public class ActifResponse
+    {
+        public int id { get; set; }
+        public decimal Valeur { get; set; }
+        public string Description { get; set; }
+        public long BorroweId { get; set; }
+
+    }
+}
