c5c7e15fd109554d67f082c569d380f938cfcd19
DOGBE Etchri Semane <124226926+Alino2023@users.noreply.github.com>
2025-04-17 15:37:08 -0400

    creation de methode privee  UploadsDirectoryExists
    Cette methode s'assure de l'existence du dossier de televersement des documents

---
+++ ApiTP1MethodeDeDev/Infrastructure/Uploading.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class Uploading
+    {
+        public static void UploadsDirectoryExists()
+        {
+            var uploadsPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads");
+            if (!Directory.Exists(uploadsPath))
+            {
+                Directory.CreateDirectory(uploadsPath);
+            }
+        }
+    }
+}
