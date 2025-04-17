3c1b7940e00a2c08dcd31bb9a8010d905be8621d
Zakaria-morjani <157501293+Zakaria-morjani@users.noreply.github.com>
2025-04-17 16:26:39 -0400

    Create ActifEntity.cs

---
+++ ApiTP1MethodeDeDev/Infrastructure/ActifEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class ActifEntity
+    {
+        public int Id { get; set; }
+        public decimal Valeur { get; set; }
+        public string Description { get; set; }
+        public long BorroweId { get; set; }
+    }
+}
