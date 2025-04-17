d289f54cb53fed2ee06d57bb08122a80f1ca8cac
Zakaria-morjani <157501293+Zakaria-morjani@users.noreply.github.com>
2025-04-17 16:26:18 -0400

    Create IActifService.cs

---
+++ ApiTP1MethodeDeDev/Domain/Actifs/IActifService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Actifs
+{
+    public interface IActifService
+    {
+        int Add(Actif actif);
+        Actif GetById(int id);
+
+
+    }
+}
