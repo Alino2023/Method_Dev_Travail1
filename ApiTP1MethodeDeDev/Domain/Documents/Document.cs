e4e5b0ac83aefb30fdddee73f29f255df838957b
DOGBE Etchri Semane <124226926+Alino2023@users.noreply.github.com>
2025-04-17 15:01:17 -0400

    La classe Document est créée

---
+++ ApiTP1MethodeDeDev/Domain/Documents/Document.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using Domain.Borrowers;
+
+namespace Domain.Documents
+{
+    public enum DocumentType
+    {
+        EmploymentProof,
+        AssetProof,
+        CreditProof
+    }
+
+    public class ProofDocument
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string FileName { get; set; }
+
+        [Required]
+        public string FilePath { get; set; } // Chemin dans le stockage ou blob
+
+        [Required]
+        public DocumentType Type { get; set; }
+
+        [Required]
+        public string FileExtension { get; set; } // .pdf ou .docx
+
+        [Required]
+        public string BorrowerSin { get; set; }
+
+        public Borrower Borrower { get; set; }
+    }
+}
+
