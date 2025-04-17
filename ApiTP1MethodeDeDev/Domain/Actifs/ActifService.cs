8545f18cdd122eaeb2d5a7f135f30257058a51f7
Zakaria-morjani <157501293+Zakaria-morjani@users.noreply.github.com>
2025-04-17 16:26:14 -0400

    Create ActifService.cs

---
+++ ApiTP1MethodeDeDev/Domain/Actifs/ActifService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Actifs
+{
+    public class ActifService : IActifRepository
+    {
+        private readonly IActifRepository _repository;
+
+        public ActifService(IActifRepository actifRepository)
+        {
+            _repository = actifRepository;
+        }
+
+        public int Add(Actif actif)
+        {
+            return _repository.Add(actif);
+        }
+
+        public Actif GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+    }
+}
