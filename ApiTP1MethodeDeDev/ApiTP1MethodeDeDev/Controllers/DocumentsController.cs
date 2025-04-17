2f9790233bbb913fd907acc05da7176dd3e9a6bc
DOGBE Etchri Semane <124226926+Alino2023@users.noreply.github.com>
2025-04-17 15:33:41 -0400

    Create DocumentsController.cs
    A compléter

---
+++ ApiTP1MethodeDeDev/ApiTP1MethodeDeDev/Controllers/DocumentsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTP1MethodeDeDev.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DocumentsController : ControllerBase
+    {
+        private readonly IWebHostEnvironment _env;
+       
+
+    }
+}
