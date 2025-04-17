d79a8a0151d61afbdeba07c74c797cf15121b78c
DOGBE Etchri Semane <124226926+Alino2023@users.noreply.github.com>
2025-04-17 15:51:56 -0400

    Update DocumentEntity.cs

--- ApiTP1MethodeDeDev/Infrastructure/DocumentEntity.cs
+++ ApiTP1MethodeDeDev/Infrastructure/DocumentEntity.cs
@@ -1,12 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Infrastructure
 {
+    public enum DocumentType
+    {
+        EmploymentProof,
+        AssetProof,
+        CreditProof
+    }
+
     public class DocumentEntity
     {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string FileName { get; set; }
+
+        [Required]
+        public string FilePath { get; set; }
+
+        [Required]
+        public DocumentType Type { get; set; }
+
+        [Required]
+        public string BorrowerSin { get; set; } // Foreign key
+
+        [ForeignKey("BorrowerSin")]
+        public Domain.Borrowers.Borrower Borrower { get; set; }
+
+        public DateTime UploadedAt { get; set; } = DateTime.UtcNow;
     }
 }
