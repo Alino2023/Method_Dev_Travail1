ccf9c6a57897cc8cd8df22bcd4fde9cedd543cbb
Zakaria-morjani <157501293+Zakaria-morjani@users.noreply.github.com>
2025-04-17 16:25:27 -0400

    Create Actif.cs

---
+++ ApiTP1MethodeDeDev/Domain/Actifs/Actif.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Actifs
+{
+    public class Actif
+    {
+        public Actif(int id, decimal valeur, string description, long borroweId)
+        {
+            Id = id;
+            Valeur = valeur;
+            Description = description;
+            BorroweId = borroweId;
+        }
+
+        public int Id { get; set; }
+        public decimal Valeur { get; set; }
+        public string Description { get; set; }
+        public long BorroweId {  get; set; }
+
+
+    }
+}
