1ac8f85d5a327e64f511402d20f30de3041cba3f
DOGBE Etchri Semane <124226926+Alino2023@users.noreply.github.com>
2025-04-17 16:21:42 -0400

    Create IDocumentService.cs

---
+++ ApiTP1MethodeDeDev/Domain/Documents/IDocumentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Domain.Documents
+{
+    public interface IDocumentService
+    {
+        Task<(bool IsSuccess, string Message)> UploadAsync(IFormFile file, string borrowerSin, string documentType);
+    }
+}
