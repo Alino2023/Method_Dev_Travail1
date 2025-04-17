aa5bce27fcd9169432cf5db2e10f2ba8a7b64f19
DOGBE Etchri Semane <124226926+Alino2023@users.noreply.github.com>
2025-04-17 16:22:23 -0400

    Create DocumentEntity.cs

---
+++ ApiTP1MethodeDeDev/Infrastructure/Documents/DocumentEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infrastructure.Documents
+{
+    public enum DocumentType
+    {
+        EmploymentProof,
+        AssetProof,
+        CreditProof
+    }
+
+    public class DocumentEntity
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string FileName { get; set; }
+
+        [Required]
+        public string FilePath { get; set; }
+
+        [Required]
+        public DocumentType Type { get; set; }
+
+        [Required]
+        public string BorrowerSin { get; set; } // Foreign key
+
+        [ForeignKey("BorrowerSin")]
+        public Domain.Borrowers.Borrower Borrower { get; set; }
+
+        public DateTime UploadedAt { get; set; } = DateTime.UtcNow;
+    }
+}
