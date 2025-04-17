4ec704473bef086308a13335d1c6bd38a484604f
Zakaria-morjani <157501293+Zakaria-morjani@users.noreply.github.com>
2025-04-17 16:26:42 -0400

    Update AppDbContext.cs

--- ApiTP1MethodeDeDev/Infrastructure/AppDbContext.cs
+++ ApiTP1MethodeDeDev/Infrastructure/AppDbContext.cs
@@ -23,6 +23,7 @@ namespace Infrastructure
         public DbSet<JobEntity> Jobs { get; set; } 
         public DbSet<LatePaymentBorrowerEntity> LatePayments { get; set; } 
         public DbSet<OtherBankLoanEntity> OtherBankLoans { get; set; } 
+        public DbSet<ActifEntity> LesActifs { get; set; }
 
 
 
