d9310828523e984f38ee7d209ff6cf33200f1eed
Zakaria-morjani <157501293+Zakaria-morjani@users.noreply.github.com>
2025-04-17 16:25:38 -0400

    Create ActifRequest.cs

---
+++ ApiTP1MethodeDeDev/ApiTP1MethodeDeDev/Dtos/ActifRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiTP1MethodeDeDev.Dtos
+{
+    public class ActifRequest
+    {
+        public int id { get; set; }
+        public decimal Valeur { get; set; }
+        public string Description { get; set; }
+        public long BorroweId { get; set; }
+    }
+}
