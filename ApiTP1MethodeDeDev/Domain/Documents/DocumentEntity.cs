fca0bfed7483b149a05beb372d2c78f5d1d22708
DOGBE Etchri Semane <124226926+Alino2023@users.noreply.github.com>
2025-04-17 16:21:26 -0400

    Create DocumentEntity.cs

---
+++ ApiTP1MethodeDeDev/Domain/Documents/DocumentEntity.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Documents;
+
+namespace Infrastructure.Services
+{
+    internal class DocumentEntity
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public DocumentType Type { get; set; }
+        public string BorrowerSin { get; set; }
+        public DateTime UploadedAt { get; set; }
+    }
+}
