58f753e4a051fe3b63c08c4ac500b4909522d1e7
Zakaria-morjani <157501293+Zakaria-morjani@users.noreply.github.com>
2025-04-17 16:26:10 -0400

    creat actifController but needs some modifications in the next time

---
+++ ApiTP1MethodeDeDev/ApiTP1MethodeDeDev/Controllers/ActifController.cs
@@ -0,0 +1,46 @@
+﻿using ApiTP1MethodeDeDev.Dtos;
+using Domain.Actifs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTP1MethodeDeDev.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ActifController : ControllerBase
+    {
+        private readonly IActifService _actifService;
+
+        public ActifController(IActifService actifService)
+        {
+            _actifService = actifService;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Actif> Get(int id)
+        {
+            var actif = _actifService.GetById(id);
+            if (actif == null)
+                return NotFound();
+
+            return actif;
+        }
+
+        [HttpPost]
+        public ActionResult<int> Post( ActifRequest actifRequest)
+        {
+            if (actifRequest == null)
+                return BadRequest("Invalid borrower data");
+
+            Actif actif = new Actif
+            (
+                 actifRequest.id,
+                 actifRequest.Valeur,
+                 actifRequest.Description,
+                 actifRequest.BorroweId
+            );
+
+            int ActifId = _actifService.Add(actif);
+            return CreatedAtAction(nameof(Get), new { Id = ActifId }, ActifId);
+        }
+    }
+}
